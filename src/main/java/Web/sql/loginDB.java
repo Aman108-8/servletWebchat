49e51c25092e56e05b01c6a7643711ba1c14b74f
Aman108-8 <aman704321@gmail.com>
2025-10-01 15:17:06 +0530

    Update loginDB.java

--- src/main/java/Web/sql/loginDB.java
+++ src/main/java/Web/sql/loginDB.java
@@ -8,7 +8,7 @@ public class loginDB {
         Connection con= null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webChat", "root", "ay704321.");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webChat", "root", "your_SQL_Password");
             
             
         }
